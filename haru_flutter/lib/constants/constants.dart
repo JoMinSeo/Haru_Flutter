959d311298db06f073844fea110c6139931d5310
Jominseo <minseocho0309@gmail.com>
2020-09-07 11:42:33 +0900

    [UPDATE] pubspec

---
+++ haru_flutter/lib/constants/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+const kMedium = TextStyle(fontFamily: "ITC");
+const kBold = TextStyle(fontFamily: "ITC", fontWeight: FontWeight.w700);
+const kExtraLight = TextStyle(fontFamily: "ITC", fontWeight: FontWeight.w300);
+const kBook = TextStyle(fontFamily: "ITC", fontWeight: FontWeight.w500);
+
+const kYellow = Color(0xFFFFC262);
+const kPink = Color(0xFFFF6C6C);
+const kPurple = Color(0xFF633CAE);
+const kRed = Color(0xFFFF000C);
